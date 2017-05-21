9574e8548e1db8f8b6f3d2136c9ce80d572bdbb9
Raquel Natali Mariz <raquel.natali95@hotmail.com>
2017-05-21 20:47:11 -0300

    Update arduinoDadosTG.ino

--- arduinoDadosTG.ino
+++ arduinoDadosTG.ino
@@ -2,26 +2,26 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include "DHT.h"
+int motionPine = 5;
+int moveu = 0;
+int estadoMovimento = LOW;
+int valorPinoMotion = 0;
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(192, 168, 1, 177);
-#define DHTPIN 2     // what digital pin we're connected to
-
-// Uncomment whatever type you're using!
-#define DHTTYPE DHT11   // DHT 11
+#define DHTPIN 2     // qual pino digital esta conectado
+#define DHTTYPE DHT11   // DHT 11 eh o tipo do sensor que estou usando
 //#define DHTTYPE DHT22  
-EthernetServer server(80);
+EthernetServer server(12345);
 DHT dht(DHTPIN, DHTTYPE);
 void setup() {
-  
+  pinMode(motionPine,INPUT);
   Serial.begin(9600);
   while (!Serial) {
     ; 
   }
-
-
-  // start the Ethernet connection and the server:
+  // comeca a conexao com a ethernet e o servidor
   Ethernet.begin(mac, ip);
   server.begin();
   Serial.print("server is at ");
@@ -31,31 +31,41 @@ void setup() {
 
 
 void loop() {
-  // listen for incoming clients
   EthernetClient client = server.available();
+  valorPinoMotion = digitalRead(motionPine);
   if (client) {
     Serial.println("new client");
-    // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
         Serial.write(c);
-        
-        //delay(2000);
       float h = dht.readHumidity();
       float t = dht.readTemperature();
       float f = dht.readTemperature(true);
       if (isnan(h) || isnan(t) || isnan(f)) {
-        Serial.println("Failed to read from DHT sensor!");
+        Serial.println("Nao conseguiu ler o sensor dht!");
         return;
       }
+     if(valorPinoMotion == HIGH){
+        if(estadoMovimento == LOW){
+          Serial.println("Movimento detectado!!");
+          moveu = 1;
+          estadoMovimento = HIGH;
+          }
+        }
+        else{
+          if(estadoMovimento == HIGH){
+            Serial.println("Movimento terminou");
+            moveu = 0;
+            estadoMovimento = LOW;
+            }
+          } 
         if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: application/json;charset=utf-8");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println("Connection: close");  // a conexao fecha apos receber a resposta
+          client.println("Refresh: 2"); // atualiza a pagina a cada cinco segundos
           client.println();
           float hif = dht.computeHeatIndex(f, h);
           float hic = dht.computeHeatIndex(t, h, false);
@@ -65,39 +75,26 @@ void loop() {
           client.print("},");  
           client.print("{\"Temperatura\": ");
           client.print(t);
+          client.print("},");
+          client.print("{\"Movimento\": ");
+          client.print(moveu);
           client.print("}");
           client.println("]");
-         /* for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-               int sensorReading = analogRead(analogChannel);
-               client.print("{\"key\": ");
-               client.print(analogChannel);                           // The key for Analog pin 0 (A0) is equal to 0   eg.  "key":0
-               client.print(", \"value\": ");
-               client.print(sensorReading);               // The value is equal to the Analog reading from that pin.  eg. "value":300
-               if(analogChannel==5){
-                client.print("}");                       // The last value will end with a bracket (without a comma)
-              }else {
-                client.print("},");                      // All other values will have a comma after the bracket.
-              }
-          }
-          client.println("]"); */
-          
+          delay(200);
           break;
         }
         if (c == '\n') {
-          // you're starting a new line
           currentLineIsBlank = true;
         }
         else if (c != '\r') {
-          // you've gotten a character on the current line
           currentLineIsBlank = false;
         }
       }
     }
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
+    // dando tempo para que o navegador recdba os dados
+    delay(200);
+    // fecha a conexao
     client.stop();
     Serial.println("client disconnected");
   }
 }
-
